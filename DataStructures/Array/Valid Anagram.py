a751f07afba36e22486a79005c07a596f0683cd5
NullDefault <davisha999@gmail.com>
2020-06-02 13:56:10 -0400

    valid anagram

---
+++ DataStructures/Array/Valid Anagram.py
@@ -0,0 +1,28 @@
+"""
+~Given two strings s and t , write a function to determine if t is an anagram of s.
+"""
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_dict = {}
+        t_dict = {}
+
+        for c in s:
+            if s_dict.get(c) is None:
+                s_dict[c] = 1
+            else:
+                s_dict[c] += 1
+
+        for c in t:
+            if t_dict.get(c) is None:
+                t_dict[c] = 1
+            else:
+                t_dict[c] += 1
+
+        return s_dict == t_dict
