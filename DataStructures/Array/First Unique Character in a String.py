bad9c9cf24a6592ac77acd0555f6aa0e37c74965
NullDefault <davisha999@gmail.com>
2020-06-02 13:44:59 -0400

    first unique character in a string

---
+++ DataStructures/Array/First Unique Character in a String.py
@@ -0,0 +1,45 @@
+"""
+~Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+"""
+
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        stack = []
+        seen_stack = []
+
+        for i in range(0, len(s)):
+            c = s[i]
+            if c in seen_stack:
+                pass
+            elif c in stack:
+                stack.pop(stack.index(c))
+                seen_stack.append(c)
+            else:
+                stack.append(c)
+
+        if len(stack) == 0:
+            return -1
+        else:
+            return s.index(stack[0])
+
+
+class CleanSolution:
+    def firstUniqChar(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        # build hash map : character and how often it appears
+        count = collections.Counter(s)
+
+        # find the index
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+        return -1
---
+++ DataStructures/Array/Two Sum.py
@@ -0,0 +1,25 @@
+"""
+~Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+~You may assume that each input would have exactly one solution, and you may not use the same element twice.
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        table = {}
+        for i in range(0, len(nums)):
+            existing_index = table.get(nums[i])
+            if existing_index is None:
+                table[nums[i]] = i
+            else:
+                if nums[i] * 2 == target:
+                    return existing_index, i
+        for key in table:
+            if table.get(target - key) is not None:
+                return table[key], table[target-key]
