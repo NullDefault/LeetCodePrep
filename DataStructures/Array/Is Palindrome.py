2d806a6897ed7dcfbadbe8efe79d7e445de792af
NullDefault <davisha999@gmail.com>
2020-06-02 14:06:58 -0400

    is palindrome

---
+++ DataStructures/Array/Is Palindrome.py
@@ -0,0 +1,19 @@
+"""
+~Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+    ~Note: For the purpose of this problem, we define empty string as valid palindrome.
+"""
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        new_s = []
+        for c in s:
+            if c.isalpha() or c.isnumeric():
+                new_s.append(c.lower())
+
+        return new_s == new_s[::-1]
